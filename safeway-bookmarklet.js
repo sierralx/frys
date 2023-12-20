d0a941a1b406065979628c921be09a80f1a92326
sierralx <sierralx@users.noreply.github.com>
2023-12-19 18:53:29 -0700

    Safeway Just For You - Automatic Coupon Clipper

---
+++ safeway-bookmarklet.js
@@ -0,0 +1,1 @@
+javascript:javascript:(function(){function sleep(e){return new Promise(t=>setTimeout(t,e))}async function click(){for(var e=document.getElementsByClassName("btn load-more")[0];void 0!==e;)e.click(),e=document.getElementsByClassName("btn load-more")[0],await sleep(500);var t=document.getElementsByClassName("grid-coupon-btn");for(i=0;i<t.length;i++)t[i].click()}click();}());
